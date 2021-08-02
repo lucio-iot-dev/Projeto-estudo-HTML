cbae6c8dd283bc0dc7aacbcff8954ae84fa86cdb
lucio <you@example.com>
2021-08-02 01:44:47 -0300

    Continuaçao da pagina,mudança do menu e criaçao de condicional no js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// DOM Document Object Model
+/* Abre e fecha o menu quando clica no ícone: Hamburguer e X */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -7,3 +7,24 @@ for (const element of toggle) {
     nav.classList.toggle('show')
   })
 }
+/* Quando clicar em um item do menu,esconder o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+/*Mudar o header da página quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header//
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header //
+    header.classList.remove('scroll')
+  }
+})
