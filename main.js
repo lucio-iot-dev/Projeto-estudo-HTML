0215d23325bafe8521b34011edc25a06af168856
lucio <you@example.com>
2021-08-07 01:34:25 -0300

    Continuaçao...Acrescimo Footer, Back-to-top e melhoria do codigo js com funçao

--- main.js
+++ main.js
@@ -16,10 +16,11 @@ for (const link of links) {
   })
 }
 /*Mudar o header da página quando der scroll*/
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header//
     header.classList.add('scroll')
@@ -27,7 +28,8 @@ window.addEventListener('scroll', function () {
     // menor que a altura do header //
     header.classList.remove('scroll')
   }
-})
+}
+
 /*Testimonials carousel slider swiper*/
 
 const swiper = new Swiper('.swiper-container', {
@@ -53,6 +55,24 @@ scrollReveal.reveal(
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+/*Back to top*/
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*When Scroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
